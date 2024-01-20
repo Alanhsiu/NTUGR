9fcec9aa103525ffbd5d5f0c339ab9fbe0f8ab3d
Alanhsiu <alan901101@gmail.com>
2024-01-21 00:23:56 +0800

    add flute

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/b09901066/ISPD-NTUEE/NTUGR/src
+# Install script for directory: /Users/alanhsiu/Desktop/Github/NTUGR/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -44,8 +39,9 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/b09901066/ISPD-NTUEE/NTUGR/build/basic/cmake_install.cmake")
-  include("/home/b09901066/ISPD-NTUEE/NTUGR/build/gr/cmake_install.cmake")
+  include("/Users/alanhsiu/Desktop/Github/NTUGR/build/basic/cmake_install.cmake")
+  include("/Users/alanhsiu/Desktop/Github/NTUGR/build/gr/cmake_install.cmake")
+  include("/Users/alanhsiu/Desktop/Github/NTUGR/build/flute/cmake_install.cmake")
 
 endif()
 
@@ -57,5 +53,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/b09901066/ISPD-NTUEE/NTUGR/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/alanhsiu/Desktop/Github/NTUGR/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "layer.h"
 #include "metrics.h"
 #include "netlist.h"
+#include "flute/flute.h"
 
 using namespace std;
 
@@ -13,8 +14,15 @@ int main(int argc, char* argv[]){
     // std::cout << "Current path is " << std::experimental::filesystem::current_path() << '\n';
 
     Design design("NTUGR");
-    design.readNet("./input/ariane133_51.net");
-    design.readCap("./input/ariane133_51.cap");
-    cout << "read netlist and capacity done" << endl;
+    // design.readNet("./input/ariane133_51.net");
+    // design.readCap("./input/ariane133_51.cap");
+    cout << "read netlist and capacity done!" << endl;
+
+    int d = 4; // 点的数量
+    DTYPE x[] = {10, 20, 30, 40}; // X坐标数组
+    DTYPE y[] = {10, 20, 30, 40}; // Y坐标数组
+
+    // Tree t = flute(d, x, y, ACCURACY);
+
     return 0;
 }
